cabf8c9ba159bf6d373650825b3e4c77b43ee96f
cresenciof <cresencio@foxlabs.io>
2020-12-06 16:29:41 -0500

    feat: added react navigation and configured dependencies for Android

--- App.tsx
+++ App.tsx
@@ -1,118 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Button, StatusBar, Text, View} from 'react-native';
+import {enableScreens} from 'react-native-screens';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, StackScreenProps} from '@react-navigation/stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+enableScreens();
 
-declare const global: {HermesInternal: null | {}};
+export const SCREEN_ONE = 'ScreenOne';
+export const SCREEN_TWO = 'ScreenTwo';
+
+type RootStackParamList = {
+  [SCREEN_ONE]: undefined;
+  [SCREEN_TWO]: undefined;
+};
+
+type ScreenOneNavigationProp = StackScreenProps<
+  RootStackParamList,
+  'ScreenOne'
+>;
+
+type ScreenTwoNavigationProp = StackScreenProps<
+  RootStackParamList,
+  'ScreenTwo'
+>;
+
+const ScreenOne: React.FC<ScreenOneNavigationProp> = (props) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Screen One</Text>
+      <Button
+        title={'Ho to Screen Two'}
+        onPress={() => {
+          props.navigation.navigate(SCREEN_TWO);
+        }}
+      />
+    </View>
+  );
+};
+
+const ScreenTwo: React.FC<ScreenTwoNavigationProp> = ({
+  navigation: {canGoBack, goBack},
+}) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Screen One</Text>
+      <Button
+        title={'Back to Screen One'}
+        onPress={() => {
+          canGoBack() && goBack();
+        }}
+      />
+    </View>
+  );
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <StatusBar barStyle="dark-content" backgroundColor={'#fff'} />
+      <NavigationContainer>
+        <RootStack.Navigator>
+          <RootStack.Screen name={SCREEN_ONE} component={ScreenOne} />
+          <RootStack.Screen name={SCREEN_TWO} component={ScreenTwo} />
+        </RootStack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
